5a2a5f897b7b93d9a57a639d62efb90b69bf15ae
Amr-Kamoun <amrkamoun2012@gmail.com>
2023-03-17 18:28:35 +0200

    Assignment 2

---
+++ Assignment 2/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        MyInteger m1 = new MyInteger(2);
+        MyInteger m2 = new MyInteger(9);
+
+        System.out.println(m1.isEven());
+        System.out.println(MyInteger.isOdd(5));
+        System.out.println(MyInteger.isPrime(m1));
+        System.out.println(m1.equals(3));
+        System.out.println(m1.equals(m2));
+    }
+}
